1701241d0b8152f4659716ccd855aea7bcba9f92
kailash aghav <aghavkailash4@gmail.com>
2026-02-26 00:15:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+#fetch the poster
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+        response = requests.get(url, timeout=10)
+        data = response.json()
+
+        if 'poster_path' in data and data['poster_path'] is not None:
+            poster_path = data['poster_path']
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except:
+        return "https://via.placeholder.com/500x750?text=Error"
+
+#recommandation system
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+# for loop movies suggesations
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+#data loading files for data loading
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+#using streamlit function for app creating
+st.title("🎬 Movie Recommendation System")
+
+movie_list = movies['title'].values
+
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button("Show Recommendation"):
+    names, posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+# for loop using for 5 movies suggestions  idea
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
